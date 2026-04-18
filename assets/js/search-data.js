9574c8cfcd9d35ff619e8816be235a648deeead8
probstlukas <thelukasprobst@gmail.com>
2026-04-18 14:14:33 +0000

    Deploying to gh-pages from @ probstlukas/lukasprobst.com@d861cae8f6fa8aaf7c6a290e5c74d4b62aec1d05 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A selection of research, academic, and technical projects.",
+          description: "A selection of academic and personal projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
